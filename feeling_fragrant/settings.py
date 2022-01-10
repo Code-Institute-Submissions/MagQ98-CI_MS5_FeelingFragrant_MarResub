8a556089ebe1aa6bef20d13158a113ddfd6ba6a8
MagQ98 <maggieq.quinlan@gmail.com>
2022-01-10 16:29:03 +0000

    Removed secret key and set debug

--- feeling_fragrant/settings.py
+++ feeling_fragrant/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zi(m^95l5-g6_$l5!@fg65k(rdb_7y+q&o7n2zqv4yf2%i_sfq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['feeling-fragrant.herokuapp.com', 'localhost']
 
