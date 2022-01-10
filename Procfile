d0962a28604aa8bfa19984450ce59d4c0f198e9f
MagQ98 <maggieq.quinlan@gmail.com>
2022-01-10 16:21:48 +0000

    Deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn feeling-fragrant.wsgi:application
--- feeling_fragrant/settings.py
+++ feeling_fragrant/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-zi(m^95l5-g6_$l5!@fg65k(rdb_7y+q&o7n2zqv4yf2%i_sfq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['feeling-fragrant.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'feeling_fragrant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
